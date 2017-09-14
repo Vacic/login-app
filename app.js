d07871ad8f306ed5ff03cc5efc6d29fc7d2b9461
Nikola Vacic <nikolavacic@ymail.com>
2017-09-14 20:50:58 +0200

    database

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var LocalStrategy = require('passport-local').Strategy;
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/loginapp');
+mongoose.connect('mongodb://vacic:123@ds135394.mlab.com:35394/login-app');
 var db = mongoose.connection;
 
 var routes = require('./routes/index');
